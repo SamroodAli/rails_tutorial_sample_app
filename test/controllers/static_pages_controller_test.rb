a962bd0df0feaa5c9dda69d7a195ffb561ab54f2
samroodAli <devsamrood@gmail.com>
2021-04-15 19:32:42 +0530

    Fix: String concatenation instead of interpolation for escaping characters

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,8 +1,7 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-
-
+  
   def setup
     @base_title = "Rails Tutorial Sample App"
   end
@@ -10,7 +9,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title","#{@base_title}"
+    assert_select "title",@base_title
   end
 
   test "should get help" do
