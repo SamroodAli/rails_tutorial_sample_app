0144ba131ccb77a5a707435bd9e790b1b73fbb87
samroodAli <devsamrood@gmail.com>
2021-04-15 16:34:17 +0530

    Test: Add tests for contacts page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -23,5 +23,10 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     get static_pages_about_url
     assert_response :success
     assert_select "title","About | #{@base_title}"
+
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title","Contact | #{@base_title}"
   end
 end
