778e806cf8b686451e926a58bcd1cdf8ebe9d108
samroodAli <devsamrood@gmail.com>
2021-04-15 16:37:25 +0530

    Static: Add static page contact for contact info

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -23,7 +23,8 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     get static_pages_about_url
     assert_response :success
     assert_select "title","About | #{@base_title}"
-
+  end
+  
   test "should get contact" do
     get static_pages_contact_url
     assert_response :success
