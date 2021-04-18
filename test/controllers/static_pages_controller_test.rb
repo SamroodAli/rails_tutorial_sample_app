8177836c4633e477be10f49dc668ba66761bc863
samroodAli <devsamrood@gmail.com>
2021-04-18 18:07:32 +0530

    Fix: Remove the home url from routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,34 +5,29 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   def setup
     @base_title = "Rails Tutorial Sample App"
   end
-
-  test "should get home" do
-    get home_url
-    assert_response :success
-    assert_select "title",@base_title
-  end
-
+  
   test "should get help" do
     get help_url
     assert_response :success
     assert_select "title","Help | #{@base_title}"
   end
-
+  
   test "should get about" do
     get about_url
     assert_response :success
     assert_select "title","About | #{@base_title}"
   end
-
+  
   test "should get contact" do
     get contact_url
     assert_response :success
     assert_select "title","Contact | #{@base_title}"
   end
-
+  
   test "should get root route" do
     get root_url
     assert_response :success
+    assert_select "title",@base_title
   end
   
 end
