27899d1d4444b94041fdb75ca80d00d391d83eea
samroodAli <devsamrood@gmail.com>
2021-04-19 23:42:49 +0530

    Test: validation test for name length

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -14,6 +14,12 @@ class UserTest < ActiveSupport::TestCase
     refute @user.valid?
   end
 
+  test 'name should not be too long' do
+    @user.name = "a" * 51
+    refute @user.valid?
+
+  end
+
   test "email should be present" do
     @user.email = '   '
     refute @user.valid?
