acaeaf75e1733c261d832ee2bc497618ec6fcaef
samroodAli <devsamrood@gmail.com>
2021-04-19 20:24:22 +0530

    Tests : Add tests for user with and without email

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(name:"Example User",email:"User@example.com")
+    @user1 = User.new(name:"Just name")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+    refute @user1.valid?
+  end
 end
