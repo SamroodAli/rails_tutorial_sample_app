ba0003099021618cf4f486ca76c2973543e70b98
samroodAli <devsamrood@gmail.com>
2021-04-20 15:42:16 +0530

    Model: Add has_secure_password method for password functionalities

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
   validates :email, presence: true, uniqueness: true,
                     length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
+
+  has_secure_password
 end
