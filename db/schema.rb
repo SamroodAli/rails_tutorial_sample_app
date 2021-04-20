5f19771859225dce1d70019e86553542f3aa44f8
samroodAli <devsamrood@gmail.com>
2021-04-20 14:10:48 +0530

    Fix: Remove fixtures at user.yml file for the testing uniqueness to pass

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_19_042534) do
+ActiveRecord::Schema.define(version: 2021_04_20_083519) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
