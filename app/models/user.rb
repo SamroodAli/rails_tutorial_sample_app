fcbb8a1e5937b66f25d6e76b611d14487cc9b22f
samroodAli <devsamrood@gmail.com>
2021-04-20 11:55:28 +0530

    Model: Add valid email regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   validates :name , presence: true, length:{maximum:50}
+  VALID_EMAIL =  /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness:true, length: {maximum:255}
 end
