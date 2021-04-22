8ce90dc0812a290e8d6270e8d9fc1fa95df5a94c
samroodAli <devsamrood@gmail.com>
2021-04-22 15:54:31 +0530

    Render show template with saved user if valid

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.valid? && @user.save
+      render 'show'
     else
       render 'new'
     end
