574dd523053973e3ed0e90fa0fa4701a7a2ac990
samroodAli <devsamrood@gmail.com>
2021-04-22 15:14:01 +0530

    Add user params method to restrict user attributes in params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,12 +8,14 @@ class UsersController < ApplicationController
   end
 
   def create
-    # @user = User.new(params[:user])
-    # if @user.save
-      #Handles a successful save
-    # else
-      # render 'new'
-    # end
+    @user = User.create(params[:user])
+    render 'new'
   end
-end
 
+  private
+  
+  def user_params
+    params.require(:user).permit(:name,:email,:password,:passowrd_confirmation)
+  end
+
+end
