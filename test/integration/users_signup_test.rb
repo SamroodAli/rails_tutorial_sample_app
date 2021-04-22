15d0107d8c2348ee97258c9c6e808a530a8e0259
samroodAli <devsamrood@gmail.com>
2021-04-22 19:41:10 +0530

    Test: get sign up page test

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -8,4 +8,9 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     @user = User.new(name:'Samrood',email:'valid@gmail.com',password:'passpass',
                      password_confirmation:'passpass')
   end
+
+  test "should get signup page" do
+    get signup_path
+    assert_response :success
+  end
 end
