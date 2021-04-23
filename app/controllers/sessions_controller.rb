d3e462ac5197bb90496c3e90943631888f864a95
samroodAli <devsamrood@gmail.com>
2021-04-23 18:40:15 +0530

    Add user params method for sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,17 @@
 class SessionsController < ApplicationController
   def new; end
+
+  def create
+    render 'new'
+  end
+
+  def destroy
+  end
+
+  private
+  def user_params
+    params.require(:sessions).permit(:email, :password)
+  end
+
+
 end
