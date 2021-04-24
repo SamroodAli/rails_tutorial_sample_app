8b43bdd9a80662679ff2a531270e7c7785cf39f8
samroodAli <devsamrood@gmail.com>
2021-04-24 12:30:17 +0530

    Add tests for invalid login attempt and flash empty for next request

---
+++ test/integration/users_login_test.rb
@@ -0,0 +1,17 @@
+require "test_helper"
+
+class UsersLoginTest < ActionDispatch::IntegrationTest
+  test "invalid login attempt" do
+    get login_path
+    assert_template "sessions/new"
+    post login_path, params: {session: {
+                              email:'',
+                              password:''
+                              }}
+    assert_template "sessions/new"
+    assert_not flash.empty?
+    assert_select "div.alert.alert"
+    get root_path
+    assert flash.empty?
+  end 
+end
