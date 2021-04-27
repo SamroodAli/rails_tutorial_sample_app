7c85dfa92234177672ee848cdfbd27a05bfa0279
samroodAli <devsamrood@gmail.com>
2021-04-27 20:21:40 +0530

    Add helper method to find current user using memoization

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: user_params[:email])
     if user && user.authenticate(user_params[:password])
-      reset_sessions
+      reset_session
       log_in user
       redirect_to user
     else  
@@ -13,7 +13,7 @@ class SessionsController < ApplicationController
     end 
   end
 
-  def destroy;ruend
+  def destroy;end
 
   private
   def user_params
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,4 +2,12 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
+
+  def current_user
+    if @current_user.nil?
+      User.find_by(id: session[:user_id])
+    else
+      @current_user
+    end
+  end
 end
