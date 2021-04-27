8dca1009a83ed9e62f06241a88761218026298a3
samroodAli <devsamrood@gmail.com>
2021-04-27 20:04:41 +0530

    Add reset_session method to prevent session fixation attacks

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: user_params[:email])
     if user && user.authenticate(user_params[:password])
+      reset_sessions
       log_in user
       redirect_to user
     else  
@@ -12,13 +13,11 @@ class SessionsController < ApplicationController
     end 
   end
 
-  def destroy
-  end
+  def destroy;ruend
 
   private
   def user_params
     params.require(:session).permit(:email, :password)
   end
-
 
 end
