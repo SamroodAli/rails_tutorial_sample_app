2657e10667bfb6c734ff62ebf6f6ff2c90ba69aa
samroodAli <devsamrood@gmail.com>
2021-05-04 21:14:30 +0530

    Session remember method for user to generate remember_token and digest

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,6 +11,12 @@ module SessionsHelper
     !current_user.nil?
   end
 
+  def remember(user)
+    user.remember
+    cookies.permanent.encrypted[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
   def log_out
     reset_session
     @current_user = nil
