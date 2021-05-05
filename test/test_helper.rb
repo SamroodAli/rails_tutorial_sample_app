e91a10d96daa7f9a37daec575f4d41c9c2e8756a
samroodAli <devsamrood@gmail.com>
2021-05-05 16:42:31 +0530

    Write test helpe log_in_as for controller and integration tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -19,9 +19,16 @@ module ActiveSupport
       !session[:user_id].nil?
     end
 
-    def log_in_is(user)
+    def log_in_as(user)
       session[:user_id] = user.id
     end
   end
 
+end
+
+class ActionDispatch::IntegrationTest
+  def log_in_as(user,password:'password',remember_me:'1') 
+    post login_path, params: {sessions: {email: user.email,password:password,
+                                        rememeber_me:remember_me}}
+  end
 end
