e93faccf8a9c2613f7b282f4e193c56d20ff1c8d
samroodAli <devsamrood@gmail.com>
2021-05-05 18:37:18 +0530

    Refactor setup method to include common remember method

--- test/helpers/sessions_helper_test.rb
+++ test/helpers/sessions_helper_test.rb
@@ -3,23 +3,22 @@ require 'test_helper'
 class SessionsHelperTest < ActionView::TestCase
   def setup
     @user = users(:samrood)
-  end
-
-  test 'testing remember and forget method' do
     remember(@user)
+  end
+  
+  test 'testing remember and forget method' do
     assert_not_nil @user.remember_digest
     forget(@user)
     assert_nil @user.remember_digest
   end
 
   test 'current user returns user when sessions is nil but cookies has user data' do
-    remember(@user)
     assert_equal @user, current_user
     assert is_logged_in?
   end
-
+  
   test 'current user returns nil when remember digest is wrong' do
-    forget(@user)
+    @user.update_attribute(:remember_digest,User.digest(User.new_token))
     assert_nil current_user
   end
 end
