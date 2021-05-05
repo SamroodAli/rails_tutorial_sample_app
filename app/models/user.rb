997ca20a74eaf73397253663fb7bc9270fc5132b
samroodAli <devsamrood@gmail.com>
2021-05-05 19:17:30 +0530

    Reuse remember_token as session_token or generate new token

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,6 +30,7 @@ class User < ApplicationRecord
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
+    remember_digest
   end
 
   def authenticated?(remember_token)
@@ -37,6 +38,11 @@ class User < ApplicationRecord
 
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
+
+  def user.session_token
+    remember_digest || remember
+  end
+
 
   def forget
     update_attribute(:remember_digest, nil)
