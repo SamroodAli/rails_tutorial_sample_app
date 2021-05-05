33558b7eb12890d86ef5412b7c2a9b334c02adbe
samroodAli <devsamrood@gmail.com>
2021-05-05 16:57:13 +0530

    Write test for logins with no remembering

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -42,7 +42,13 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 
   test 'login with remembering' do 
       log_in_as(@user,remember_me: '1')
-      puts 'hello bro, debug calmly',cookies[:remember_token]
       assert_not cookies[:remember_token].blank?
   end
+
+  test 'login with no remembering' do 
+      log_in_as(@user,remember_me: '0')
+      assert cookies[:remember_token].blank?
+  end
+
+
 end
