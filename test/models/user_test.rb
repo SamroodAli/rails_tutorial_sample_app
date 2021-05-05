7e1dd0de172593560b4eca20bc4c558e4918f9ec
samroodAli <devsamrood@gmail.com>
2021-05-05 08:42:43 +0530

    Write test for nil class user authentication

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -82,4 +82,9 @@ class UserTest < ActiveSupport::TestCase
     @user.password = @user.password_confirmation = 'a' * 7
     refute @user.valid?
   end
+
+
+  test 'authenticated should return false for a user with nil digest' do 
+    assert_not @user.authenticated?('')
+  end
 end
