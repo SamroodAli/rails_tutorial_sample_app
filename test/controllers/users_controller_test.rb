1d52fcf80fdbcc0961e23efa39e0f63adf12c84f
samroodAli <devsamrood@gmail.com>
2021-05-06 18:34:45 +0530

    Write test for catching differnet users editing other users

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   def setup
-    @user = users(:samrood)
+    @user1 = users(:samrood)
+    @user2 = users(:micheal)
   end
 
   test 'should get new' do
@@ -11,12 +12,12 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should get edit page only when logged in' do
-    get edit_user_path(@user)
+    get edit_user_path(@user1)
     assert_redirected_to login_url
   end
 
-  test 'should update user only when logged in' do
-    patch user_path(@user), params: { user: {
+  test 'should update user1 only when logged in' do
+    patch user_path(@user1), params: { user: {
       name: 'Samrood Ali',
       email: 'example2@gmail.com',
       password: '',
@@ -25,5 +26,16 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
     assert_redirected_to login_url
   end
+
+
+  test 'should redirect when logged in as another user' do
+    log_in_as(@user2)
+    patch user_path(@user1), params: { user: {
+      name: 'Samrood Ali',
+      email: 'example2@gmail.com',
+      password: '',
+      password_confirmation: ''
+    } }
+    assert_redirected_to login_url
   end
 end
