1d3942b76125cd33eb34a5fa8e0533121db6296a
samroodAli <devsamrood@gmail.com>
2021-05-06 15:56:31 +0530

    Update unsuccessful test to test flash messages and correct render

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -11,6 +11,14 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   test 'unsuccessful edit' do 
     get edit_user_path(@user)
     assert_response :success
+    patch user_path(@user), params: {user: {
+                              name: @user.name, 
+                              email:'invalid_email',
+                              password:'somepass',
+                              password_confirmation:'nonmatchingpass'}}
+    assert_template 'users/edit'
+    assert_select 'div.alert'
+    assert_select 'div#error_explanation li',2
   end
 
 end
