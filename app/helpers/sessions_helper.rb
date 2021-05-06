8669d1376f2b1b97018310e7c604983949af1bd4
samroodAli <devsamrood@gmail.com>
2021-05-06 20:19:18 +0530

    Write forwarding back method for friendly forwarding

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -46,4 +46,9 @@ module SessionsHelper
   def store_location
     session[:forwarding_url] = request.original_url if request.get?
   end
+
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+  end
+
 end
