109fcfbd4860ce6cbc41dff484b37e48a5114eab
samroodAli <devsamrood@gmail.com>
2021-05-06 19:34:48 +0530

    Correct user before action filter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :login_required, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
   def show
     @user = User.find(params[:id])
   end
@@ -45,5 +46,11 @@ class UsersController < ApplicationController
       flash[:danger] = 'PLease log in'
       redirect_to login_url
     end
+  end
+
+  def correct_user
+    @user =  User.find_by(id: params[:id])
+    flash[:danger] = "Wrong user, please sign in to your account"
+    redirect_to login_url unless @user == current_user
   end
 end
