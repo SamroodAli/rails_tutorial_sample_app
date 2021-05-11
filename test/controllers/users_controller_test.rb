d0049acc74e8240af0697e4659872f642aa9ce48
samroodAli <devsamrood@gmail.com>
2021-05-11 16:54:07 +0530

    Test:Test delete option for admins

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,8 +2,8 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   def setup
-    @user1 = users(:samrood)
-    @user2 = users(:micheal)
+    @admin = users(:samrood)
+    @other_user = users(:micheal)
   end
 
   test 'should redirect index when not logged in' do
@@ -17,12 +17,12 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should get edit page only when logged in' do
-    get edit_user_path(@user1)
+    get edit_user_path(@admin)
     assert_redirected_to login_url
   end
 
-  test 'should update user1 only when logged in' do
-    patch user_path(@user1), params: { user: {
+  test 'should update admin only when logged in' do
+    patch user_path(@admin), params: { user: {
       name: 'Samrood Ali',
       email: 'example2@gmail.com',
       password: '',
@@ -33,8 +33,8 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should redirect when logged in as another user' do
-    log_in_as(@user2)
-    patch user_path(@user1), params: { user: {
+    log_in_as(@other_user)
+    patch user_path(@admin), params: { user: {
       name: 'Samrood Ali',
       email: 'example2@gmail.com',
       password: '',
@@ -45,17 +45,24 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
     test 'should redirect destroy when not logged in' do
       assert_no_difference 'User.count' do
-        delete user_path(@user1)
+        delete user_path(@admin)
       end
       assert_redirected_to login_url
     end
 
-    test 'should delete user when logged in as admin' do
-      log_in_as(@user2)
+    test 'should redirect when logged in as admin' do
+      log_in_as(@other_user)
       assert_no_difference 'User.count' do
-        delete user_path(@user1)
+        delete user_path(@admin)
       end
       assert_redirected_to root_path
     end
 
+    test 'should delete user when logged in as admin' do
+      log_in_as(@admin)
+      assert_difference 'User.count', -1 do
+        delete user_path(@other_user)
+      end
+      assert_redirected_to users_path
+    end
 end
