4d4d66cd1669aa0615fd84e69a7edb219d4ffc85
samroodAli <devsamrood@gmail.com>
2021-05-11 16:35:18 +0530

    Add:Admin true to fixture users first user samrood

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -64,4 +64,8 @@ class UsersController < ApplicationController
     @user = User.find_by(id: params[:id])
     redirect_to login_url unless correct_user?(@user)
   end
+
+  def admin_user
+    redirect_to root_url unless current_user.admin?
+  end
 end
