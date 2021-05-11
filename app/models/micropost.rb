fced6ce8744b24dbc928f97b5445d95795d5328d
samroodAli <devsamrood@gmail.com>
2021-05-11 21:14:55 +0530

    Feat:Usign Proc.new to order elements

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,6 @@
 class Micropost < ApplicationRecord
   belongs_to :user, dependent: :destroy
-  default_scope -> { order(created_at: :desc) }
+  default_scope(Proc.new{ order(created_at: :desc) })
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140}
 
