9684a7ab06f8d75542e6cd2bb79c977eb3729158
samroodAli <devsamrood@gmail.com>
2021-05-11 17:00:41 +0530

    Test:Add tests for no delete links for non admin users

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -2,15 +2,23 @@ require "test_helper"
 
 class UsersIndexTest < ActionDispatch::IntegrationTest
   def setup
-    @user = users(:samrood)
+    @admin = users(:samrood)
+    @other_user = users(:micheal)
   end
   test 'index including pagination' do
-    log_in_as(@user)
+    log_in_as(@admin)
     get users_path
     assert_template 'users/index'
     assert_select 'div.pagination',count:2
     User.paginate(page: 1).each do |user|
-      assert_select 'a[href=?]',user_path(@user),text: @user.name
+      assert_select 'a[href=?]',user_path(@admin),text: @admin.name
     end
+  end
+
+
+  test 'index as non admin' do
+    log_in_as(@other_user)
+    get users_path
+    assert_select 'a', text:'delete',count:0
   end
 end
