3fe7492899ad8a29a49206437f16b86021780cb9
samroodAli <devsamrood@gmail.com>
2021-05-11 16:13:17 +0530

    Fix:Remove admin from user params strong parameters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,7 +40,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation,:admin)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
   def login_required
