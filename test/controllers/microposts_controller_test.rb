78a6be7c71aea4552737ed6cea341f91bb97b1f0
samroodAli <devsamrood@gmail.com>
2021-05-12 14:24:37 +0530

    Fix:Microposts fixture not enuf posts for pagination issue

--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class MicropostsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @micropost = microposts(:orange)
+  end
+
+
+  test 'should redirect create when not logged in' do
+    assert_no_difference 'Micropost.count' do
+      post :create, micropost: {content:  "Lorem ipsum"}
+    end
+    assert_redirected_to login_url
+  end
 end
