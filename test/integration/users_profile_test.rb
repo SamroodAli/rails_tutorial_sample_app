813a6167cd5cde4ae8b2f334c4e859b9f4dd23d0
samroodAli <devsamrood@gmail.com>
2021-05-12 12:05:58 +0530

    Test:Add assertions for profile display including assertion_match for microposts count

---
+++ test/integration/users_profile_test.rb
@@ -0,0 +1,17 @@
+require "test_helper"
+
+class UsersProfileTest < ActionDispatch::IntegrationTest
+  include ApplicationHelper
+  def setup
+    @user = users(:samrood)
+  end
+
+  test 'profile display' do
+    get user_path(@user)
+    assert_template 'users/show'
+    assert_select 'title',full_title(@user.name)
+    assert_select 'h1', text:@user.name
+    assert_select 'h1 img.gravatar'
+    assert_match @user.microposts.count.to_s, response.body
+  end
+end
