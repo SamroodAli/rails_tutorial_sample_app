1c36289f623276027aa86c363ae709287acecee4
samroodAli <devsamrood@gmail.com>
2021-05-12 18:40:27 +0530

    Referrer:Destroy action before filter, destroy method and redirect to referrer

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
   before_action :login_required, only: [:create, :destroy]
+  before_action :correct_user, only: :destroy
 
   def create
     @micropost = current_user.microposts.build(microposts_params)
@@ -13,11 +14,20 @@ class MicropostsController < ApplicationController
   end
 
   def destroy
+    store_original_url
+    @micropost.destroy
+    flash[:success] = "Micropost deleted"
+    redirect_to request.referrer
   end
 
   private
   def microposts_params
     params.require(:micropost).permit(:content)
+  end
+
+  def correct_user
+    @micropost = current_user.microposts.find_by(id: params[:id])
+    redirect_to login_url if @micropost.nil?
   end
 
 end
