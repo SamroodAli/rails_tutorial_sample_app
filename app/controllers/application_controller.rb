721bf2295c85d36645e52810b86bcb26a4172fec
samroodAli <devsamrood@gmail.com>
2021-05-12 14:48:00 +0530

    Strong_parameters:Permit only content in params for microposts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
+
+
+  def login_required
+    unless logged_in?
+      store_original_url
+      flash[:danger] = 'PLease log in'
+      redirect_to login_url
+    end
+  end
+
 end
--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,10 +1,16 @@
 class MicropostsController < ApplicationController
-  before_action :logged_in_user, only: [:create, :destroy]
+  before_action :login_required, only: [:create, :destroy]
 
   def create
+    @micropost = current_user.micropost.build(microposts_params)
   end
 
   def destroy
+  end
+
+  private
+  def microposts_params
+    params.require(:micropost).permit(:content)
   end
 
 end
