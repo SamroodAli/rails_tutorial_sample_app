ace6b012a7c0093296b6bc4eff1a24bbe983db0b
samroodAli <devsamrood@gmail.com>
2021-05-12 14:32:07 +0530

    Fix:Fix tests path in microposts controller test

--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -8,14 +8,14 @@ class MicropostsControllerTest < ActionDispatch::IntegrationTest
 
   test 'should redirect create when not logged in' do
     assert_no_difference 'Micropost.count' do
-      post :create, params: { micropost: {content:  "Lorem ipsum"}}
+      post microposts_path , params: { micropost: {content:  "Lorem ipsum"}}
     end
     assert_redirected_to login_url
   end
 
   test 'should redirect destroy when not logged in' do
     assert_no_difference 'Micropost.count' do
-      delete :destroy, params:{id:@micropost}
+      delete micropost_path(@micropost), params:{id:@micropost}
     end
     assert_redirected_to login_url
   end
