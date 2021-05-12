ec78c74ef4814d23219efc7efb14563cf3b3e89f
samroodAli <devsamrood@gmail.com>
2021-05-12 14:27:28 +0530

    Fix:Write test when users are not logged in and try to destroy users

--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -12,4 +12,12 @@ class MicropostsControllerTest < ActionDispatch::IntegrationTest
     end
     assert_redirected_to login_url
   end
+
+  test 'should redirect destroy when not logged in' do
+    assert_no_difference 'Micropost.count' do
+      delete :destroy, params:{id:@micropost}
+    end
+    assert_redirected_to login_url
+  end
+
 end
