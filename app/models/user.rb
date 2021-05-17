8fffec6fb3377b321e8c3efd35faa3a3be69894c
samroodAli <devsamrood@gmail.com>
2021-05-17 21:08:24 +0530

    Association:Between following and user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
   has_many :active_relationships, class_name: :Relationship, foreign_key: :follower_id, dependent: :destroy
+
+  has_many :following, through: :active_relationships, source: :followed
+
   attr_accessor :remember_token
 
   before_save { email.downcase! } # same as self.email = self.email.downcase or self.email = email.downcase
