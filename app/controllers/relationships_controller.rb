0c4e69013b68cc620eb303be32667c2509dd0f84
samroodAli <devsamrood@gmail.com>
2021-05-18 20:42:15 +0530

    Test:Test relationship count when not logged in

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,2 +1,11 @@
 class RelationshipsController < ApplicationController
+
+
+  test 'should redirect create when not logged in' do
+    assert_not difference 'Relationship.count' do
+      post relationships_path
+    end
+    assert_redirected_to login_url
+  end
+
 end
