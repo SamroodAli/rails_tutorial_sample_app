acb40fbb217450c8e836e17d4618ca4ccbc3e214
samroodAli <devsamrood@gmail.com>
2021-05-18 18:44:26 +0530

    Fix:Typo , user to users in user followers action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,7 +55,7 @@ class UsersController < ApplicationController
   def followers
     @title = "Followers"
     @user = User.find(params[:id])
-    @user = @user.followers.paginate(page:params[:page])
+    @users = @user.followers.paginate(page:params[:page])
     render 'show_follow'
   end
 
